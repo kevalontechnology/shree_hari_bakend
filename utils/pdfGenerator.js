6ee8b580160d763e8e44926c72f981d902119057
Jeel Popat <jeelpopat1010@gmail.com>
2026-08-01 18:08:22 +0530

    pdf

--- utils/pdfGenerator.js
+++ utils/pdfGenerator.js
@@ -1,6 +1,16 @@
 const puppeteer = require('puppeteer');
 const { generateInrInvoicePdf } = require('../services/pdfService');
 const { renderVgmHtml } = require('../templates/vgmTemplate');
+const puppeteer = require('puppeteer-core');
+const chromium = require('@sparticuz/chromium');
+
+const browser = await puppeteer.launch({
+  args: chromium.args,
+  defaultViewport: chromium.defaultViewport,
+  executablePath: await chromium.executablePath(),
+  headless: chromium.headless,
+  ignoreHTTPSErrors: true,
+});
 
 const generateInvoicePDF = async (shipment, exporter) => {
   return await generateInrInvoicePdf({
